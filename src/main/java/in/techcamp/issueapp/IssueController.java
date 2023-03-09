66e04b1dd4550af0e5faff38f6d99942a88a8855
miyazato1872 <miyazato5997@gmail.com>
2023-03-10 01:41:13 +0900

    【一覧機能実装】 ・index.html作成 ・Controllerのimport org漏れを追加 import org.springframework.ui.Model;

--- src/main/java/in/techcamp/issueapp/IssueController.java
+++ src/main/java/in/techcamp/issueapp/IssueController.java
@@ -1,7 +1,9 @@
 package in.techcamp.issueapp;
 
 import lombok.AllArgsConstructor;
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
