c84ce87a838d0f3db21aff41631960466036d59f
miyazato1872 <miyazato5997@gmail.com>
2023-03-10 14:24:11 +0900

    更新機能の実装 削除機能の実装 アプリ完成

--- src/main/java/in/techcamp/issueapp/IssueAppApplication.java
+++ src/main/java/in/techcamp/issueapp/IssueAppApplication.java
@@ -9,5 +9,4 @@ public class IssueAppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(IssueAppApplication.class, args);
 	}
-
 }
--- src/main/java/in/techcamp/issueapp/IssueController.java
+++ src/main/java/in/techcamp/issueapp/IssueController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -30,5 +31,24 @@ public class IssueController {
         var issueList = issueRepository.findAll();  //『var』を使うと値の種類によってデータ型が推論され、推論されたデータ型で宣言が行われ「型推論」
         model.addAttribute("issueList", issueList);
         return "index";
+    }
+
+    @GetMapping("/issues/{id}")
+    public String issueDetail(@PathVariable long id, Model model){
+        var issue = issueRepository.findById(id);
+        model.addAttribute("issue", issue);
+        return "detail";
+    }
+
+    @PostMapping("/issues/{id}/update")
+    public String updateIssue(@PathVariable long id, IssueForm issueForm){
+        issueRepository.update(id, issueForm.getTitle(), issueForm.getContent(), issueForm.getPeriod(), issueForm.getImportance());
+        return "redirect:/";
+    }
+
+    @PostMapping("issues/{id}/delete")
+    public String deleteIssue(@PathVariable Long id){
+        issueRepository.deleteById(id);
+        return "redirect:/";
     }
 }
